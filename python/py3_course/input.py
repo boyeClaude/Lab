7892ae5b499428fbbdec2caeb014c4aab39208bb
boyeClaude <boye.claude@gmail.com>
2025-10-14 03:36:22 +0000

    Add python folder and subfolders related to python
    - add a folder and subfolders. Those subfolders are related to python
      codes or scripts from my learning

---
+++ python/py3_course/input.py
@@ -0,0 +1,11 @@
+#name =  input("Enter your name: ")
+#print("Hello, " + name)
+
+#We get a string from input function, so cast into a number we have to use 
+#int function
+
+#numb = int(input("Enter a Number"))
+#print(numb)
+
+print(16 - 2 * 5 // 3 + 1)
+print(int(53.785))
---
+++ python/py3_course/type_and_type_conversion.py
@@ -0,0 +1,3 @@
+#Know the type of the variable and convert it to string
+print(type(str(17)))
+print("17")
