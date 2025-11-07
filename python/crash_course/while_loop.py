b4e30021e19f5ab2c4e838409c25d10dc4a619aa
boyeClaude <boye.claude@gmail.com>
2025-11-07 22:49:12 +0000

    Methods on strings and loops
    - use methods on string as slice, append, delete
    - learning about while loop on google crash course

---
+++ python/crash_course/while_loop.py
@@ -0,0 +1,4 @@
+x = 1
+while (x <=5):
+    print(f"Value of x {x}")
+    x += 1
--- python/py3_course/string_methods.py
+++ python/py3_course/string_methods.py
@@ -9,3 +9,15 @@ price = int(input("enter your price: "))
 
 print(f"Your name is {name} and your price is {price}")
 print("Your name is {} and your price is {:.2f}".format(name, price))
+
+
+s = "I saw the movie, Mary Poppins Returns, and I thought it was great."
+print(len(s))
+
+
+sent = "The mall has excellent sales right now."
+wrds = sent.split()
+wrds[1] = 'store'
+new_sent = " ".join(wrds)
+
+print(new_sent)
